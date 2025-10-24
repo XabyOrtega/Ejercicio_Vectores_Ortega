1647a7dc04a3ff3e2b38797b91540f5ee860436a
Xaby Ortega <xaby.ortega@udla.edu.ec>
2025-10-24 14:00:10 -0500

    Vector lleno de numeros aleatorios

--- Vector.c
+++ Vector.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
     int tamano;
-    int Ortega[100] ;
+    int Ortega[100];
 
     printf("Ingrese el tamaño del vector (máximo 100): ");
     scanf("%d", &tamano);
@@ -12,7 +14,17 @@ int main() {
         return 1;
     }
 
-    printf("El tamaño ingresado del vector Ortega fue: %d\n", tamano);
+    srand(time(NULL));
+
+    for (int i = 0; i < tamano; i++) {
+        Ortega[i] = rand() % 100;
+    }
+
+    printf("Vector Ortega con números aleatorios:\n");
+    for (int i = 0; i < tamano; i++) {
+        printf("%d ", Ortega[i]);
+    }
+    printf("\n");
 
     return 0;
 }
